a7d332fcd6836fd339e9ec2790f9b93f7aa45a28
IgorDias1998 <igordiass1998@gmail.com>
2025-04-05 21:30:35 -0300

    Commit inicial - Models
    Criação da branch desenvolvimento para testes e novas funcionalidades

---
+++ SisONG/Models/Evento.cs
@@ -0,0 +1,33 @@
+﻿namespace SisONG.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Evento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Titulo { get; set; }
+
+        [StringLength(500)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public DateTime DataHora { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Local { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        // Relação muitos-para-muitos com Voluntário
+        public ICollection<Voluntario> VoluntariosInscritos { get; set; } = new List<Voluntario>();
+    }
+
+}

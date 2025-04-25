53119cc00d4019f86c7da132b58b4435921a49e3
IgorDias1998 <igordiass1998@gmail.com>
2025-04-24 23:25:34 -0300

    Criação EndPoint ProdutoInsumo - Permite o cadastro de produtos físicos - Controller - Profile - Repository - Service

---
+++ SisONG/Repositories/ProdutoInsumoRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SisONG.Data.Context;
+using SisONG.Models;
+
+namespace SisONG.Repositories
+{
+    public class ProdutoInsumoRepository : IProdutoInsumoRepository
+    {
+        private readonly SisONGContext _context;
+
+        public ProdutoInsumoRepository(SisONGContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ProdutoInsumo>> GetAllAsync()
+        {
+            return await _context.ProdutosInsumos.ToListAsync();
+        }
+
+        public async Task<ProdutoInsumo> GetByIdAsync(int id)
+        {
+            return await _context.ProdutosInsumos.FindAsync(id);
+        }
+
+        public async Task CreateAsync(ProdutoInsumo produto)
+        {
+            await _context.ProdutosInsumos.AddAsync(produto);
+        }
+
+        public async Task UpdateAsync(ProdutoInsumo produto)
+        {
+            _context.ProdutosInsumos.Update(produto);
+        }
+
+        public async Task DeleteAsync(ProdutoInsumo produto)
+        {
+            _context.ProdutosInsumos.Remove(produto);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
